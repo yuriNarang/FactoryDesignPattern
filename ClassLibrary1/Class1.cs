3871706c2947188b1600321413e3d22c33e3fe08
Yuri Narang <yuri.narang@nagarro.com>
2020-07-12 16:58:08 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void CreateFile()
+        {
+            using(TextWriter w = File.CreateText("log.txt"))
+            {
+                w.WriteLine("This is line one");
+            }
+        }
+    }
+}
